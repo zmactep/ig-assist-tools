142b91b0069013641e5406811d7e87141cd0b988
zmactep <zhmactep@gmail.com>
2013-06-28 10:49:01 +0400

    Test changes

--- imapreduce.py
+++ imapreduce.py
@@ -6,16 +6,22 @@ from ighumanizer3.extra.svm.svm_type import *
 logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
                     level=logging.DEBUG, filename="test_log.log")
 
+logging.debug("Client creation.")
 rc = Client()
 dview = rc[:]
+logging.debug("OS import.")
 dview.execute("import os").get()
 
-dview.apply(lambda: os.path.abspath(os.curdir)).get()
-dview.apply(lambda: os.chdir("/home/yakovlev/devel/ig-assist-tools")).get()
-dview.apply(lambda: os.path.abspath(os.curdir)).get()
 
+cd = dview.apply(lambda: os.path.abspath(os.curdir)).get()
+logging.debug("Current dir:" + str(cd))
+dview.apply(lambda: os.chdir("/home/yakovlev/devel/ig-assist-tools")).get()
+cd = dview.apply(lambda: os.path.abspath(os.curdir)).get()
+logging.debug("Current dir (after changing):" + str(cd))
 dview.execute("from ighumanizer3.extra.svm.svm_type import *").get()
 
 dview.push({'test': test}).get()
 
+logging.debug("Test started!")
 res = dview.map(test, range(7, 11))
+print(res)
